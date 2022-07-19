f13368121b93ece8df9de2c455489564e36bbf75
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-19 20:19:31 +0200

    removed requirements.txt

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+mkdir ./simulation_output/
+
+
+
+./NanoSim/src/read_analysis.py genome -i ./data/MT-MTPHL-3995074_R1.fastq -rg ./data/reference_genome.fasta
+
+
+
+#output/train_model.sh &&/home/vickylara/Documents/Covid-SpiNGS/NanoSim/src/simulator.py metagenome -gl /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/metagenome_covid_human.tsv -dl /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/dna_type_list.tsv -a /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/abundance_covid_extreme_case.tsv -o new_extreme_simulated_covid -t 8&& duration=$SECONDS && echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." >> log.txt &&
+output/simulate.sh -o /new_simulation_output/
+
+
+/home/vickylara/Documents/Covid-SpiNGS/NanoSim/src/simulator.py metagenome -gl ./config_files/metagenome_covid_human.tsv -dl /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/dna_type_list.tsv -a /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/abundance_covid_extreme_case.tsv -o new_extreme_simulated_covid -t 8&& duration=$SECONDS && echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." >> log.txt &&
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 setupDirs(){
-  mkdir "exampleData"
-  mkdir "exampleData"/"humanGenome" && mkdir "exampleData"/"covidReference"
+  mkdir "data"
+  mkdir "data"/"humanGenome" && mkdir "data"/"covidReference"
 }
 
 downloadHumanGenome(){
@@ -10,18 +10,18 @@ downloadHumanGenome(){
   for i in {1..22}
     do
       echo $i
-        wget "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/chr"$1".fa.gz" -P .exampleData/humanGenome
+        wget "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/chr"$1".fa.gz" -P .data/humanGenome
         extract "$i"
     done
 }
 
 downloadCovidReference(){
-  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P .exampleData/covidReference
-  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P .exampleData/covidReference
+  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P .data/covidReference
+  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P .data/covidReference
 }
 
 extract(){
-  gzip -d "./exampleData/humanGenome/chr"$1".fa.gz"
+  gzip -d "./data/humanGenome/chr"$1".fa.gz"
 }
 
 setupNanosim(){
@@ -29,7 +29,7 @@ setupNanosim(){
     git submodule init
     git submodule update
     cp fixed_requirements.txt ./NanoSim/requirements.txt
-    # Solution
+    # Solution for this ??
     #cd NanoSim %% conda create --name nanosim python=3.7
     #conda activate nanosim
     #conda install --file requirements.txt -c conda-forge -c bioconda
