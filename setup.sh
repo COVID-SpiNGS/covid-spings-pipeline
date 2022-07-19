b17f43b9cbd92f8cb04351afd0f473960be4e536
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-19 20:37:28 +0200

    Added download links and renamed dirs

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 setupDirs(){
   mkdir "data"
-  mkdir "data"/"humanGenome" && mkdir "data"/"covidReference"
+  mkdir "data"/"humanGenome" && mkdir "data"/"covid"
 }
 
 downloadHumanGenome(){
@@ -15,11 +15,11 @@ downloadHumanGenome(){
     done
 }
 
-downloadCovidReference(){
+downloadCovid(){
 
-  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P .exampleData/covidReference
-  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P .exampleData/covidReference
-  wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P .exampleData/covidReference
+  #wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P .data/covid/
+  #wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P .data/covid/
+  wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P .data/covid/
 }
 
 extract(){
@@ -40,5 +40,5 @@ setupNanosim(){
 
 setupDirs
 #downloadHumanGenome
-#downloadCovidReference
+downloadCovid
 setupNanosim
