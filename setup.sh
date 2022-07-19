0e8a20eda2abcb4a18a98b0fc3c67bedfa1724ce
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-19 19:59:46 +0200

    Split up script into functions

--- setup.sh
+++ setup.sh
@@ -1,43 +1,41 @@
 #!/bin/bash
 
-download(){
-  wget "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/chr"$1".fa.gz" -P .exampleData/humanGenome
+setupDirs(){
+  mkdir "exampleData"
+  mkdir "exampleData"/"humanGenome" && mkdir "exampleData"/"covidReference"
+}
+
+downloadHumanGenome(){
+
+  for i in {1..22}
+    do
+      echo $i
+        wget "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/chromosomes/chr"$1".fa.gz" -P .exampleData/humanGenome
+        extract "$i"
+    done
+}
+
+downloadCovidReference(){
+  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P .exampleData/covidReference
+  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P .exampleData/covidReference
 }
 
 extract(){
-  gzip -d "./humanGenome/chr"$1".fa.gz"
+  gzip -d "./exampleData/humanGenome/chr"$1".fa.gz"
 }
 
 setupNanosim(){
 
     git submodule init
     git submodule update
-
-    #git clone https://github.com/bcgsc/NanoSim.git &&
-    cp requirements.txt ./NanoSim/requirements.txt
+    cp fixed_requirements.txt ./NanoSim/requirements.txt
     #cd NanoSim %% conda create --name nanosim python=3.7
     #conda activate nanosim
     #conda install --file requirements.txt -c conda-forge -c bioconda
     #cd ..
 }
 
-
-#curl -s https://get.nextflow.io | bash
-mkdir "exampleData"
-mkdir "exampleData"/"humanGenome" && mkdir "exampleData"/"covidReference" #&& cd ./humanGenome
-
-
-for i in {1..22}
-  do
-    echo $i
-    #download "$i"
-    #extract "$i"
-  done
-
-
-
-
-#000sudo mkdir /new_simulation_output/#output/train_model.sh && output/simulate.sh -o /new_simulation_output/
+setupDirs
+#downloadHumanGenome
+#downloadCovidReference
 setupNanosim
-
-#./nextflow main.nf --wd $PWD
