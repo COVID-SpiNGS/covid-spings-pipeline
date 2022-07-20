8e981f427cb205fb7a5e902fde851779a90c907e
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-21 01:29:07 +0200

    Fixed typo in setup script

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ data_dir="$(dirname $(realpath $0) )"
 echo $data_dir
 
 #Train
-#../NanoSim/src/read_analysis.py genome -i "$data_dir/data/covid/SP-2_R1.fastq" -rg "$data_dir/data/covid/SARS-CoV-2_MSA_file1.fasta"
+../NanoSim/src/read_analysis.py genome -i "$data_dir/data/covid/SP-2_R1.fastq" -rg "$data_dir/data/covid/SARS-CoV-2_MSA_file1.fasta"
 #Simulate
 #../NanoSim/src/simulator.py metagenome -gl "$data_dir/config_files/metagenome_covid_human.tsv" -dl "$data_dir/config_files/dna_type_list.tsv" -a "$data_dir/config_files/abundance_covid.tsv"
--- setup.sh
+++ setup.sh
@@ -19,10 +19,10 @@ downloadCovid(){
 
   wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P ./data/covid/
   wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P ./data/covid/
-  mv ./data/covid/SP-2_R1.fastq.1 ./data/covid/SP-1_R2.fastq
+  mv ./data/covid/SP-2_R1.fastq.1 ./data/covid/SP-2_R1.fastq
   mv ./data/covid/SP-2_R2.fastq.1 ./data/covid/SP-2_R2.fastq
-  wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P ./data/covid/
-  mv ./data/covid/5ede595708aad3013143c7f3?action=download\&direct\&version=3 ./data/covid/SARS-CoV-2_MSA_file1.fasta
+  #wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P ./data/covid/
+  #mv ./data/covid/5ede595708aad3013143c7f3?action=download\&direct\&version=3 ./data/covid/SARS-CoV-2_MSA_file1.fasta
 }
 
 extract(){
