07f8bab881d82326dcb71b3029dff379a5d8ab6a
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-21 00:44:12 +0200

    Added dynamic path

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 mkdir ./"NanoSim_output"/ && cd NanoSim_output
 
 
-data_dir="$(dirname $(dirname $(realpath $0)) )/data"
+data_dir="$(dirname $(realpath $0) )"
 echo $data_dir
 
 #Train
-../NanoSim/src/read_analysis.py genome -i "$data_dir/covid/SP-2_R1.fastq" -rg "$data_dir/covid/SARS-CoV-2_MSA_file1.fasta"
+#../NanoSim/src/read_analysis.py genome -i "$data_dir/data/covid/SP-2_R1.fastq" -rg "$data_dir/data/covid/SARS-CoV-2_MSA_file1.fasta"
 #Simulate
-#../NanoSim/src/simulator.py metagenome -gl ../config_files/metagenome_covid_human.tsv -dl ../config_files/dna_type_list.tsv -a ../config_files/abundance_covid.tsv
+#../NanoSim/src/simulator.py metagenome -gl "$data_dir/config_files/metagenome_covid_human.tsv" -dl "$data_dir/config_files/dna_type_list.tsv" -a "$data_dir/config_files/abundance_covid.tsv"
