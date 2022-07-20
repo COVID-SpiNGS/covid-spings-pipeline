d1837be5fe5f482abad0d24eb39b2d0f89309884
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-20 23:45:01 +0200

    Fixed path issues in setup script

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-mkdir ./simulation_output/
 
+mkdir ./"NanoSim_output"/ && cd NanoSim_output
 
-
-./NanoSim/src/read_analysis.py genome -i ./data/MT-MTPHL-3995074_R1.fastq -rg ./data/SARS-CoV-2_MSA_file1.fasta
-
-
-
-#output/train_model.sh &&/home/vickylara/Documents/Covid-SpiNGS/NanoSim/src/simulator.py metagenome -gl /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/metagenome_covid_human.tsv -dl /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/dna_type_list.tsv -a /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/abundance_covid_extreme_case.tsv -o new_extreme_simulated_covid -t 8&& duration=$SECONDS && echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." >> log.txt &&
-output/simulate.sh -o /new_simulation_output/
-
-
-/home/vickylara/Documents/Covid-SpiNGS/NanoSim/src/simulator.py metagenome -gl ./config_files/metagenome_covid_human.tsv -dl /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/dna_type_list.tsv -a /home/vickylara/Documents/Covid-SpiNGS/NanoSim/config_files/abundance_covid_extreme_case.tsv -o new_extreme_simulated_covid -t 8&& duration=$SECONDS && echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." >> log.txt &&
+#Train
+../NanoSim/src/read_analysis.py genome -i ../data/SP-2_R1.fastq -rg ../data/SARS-CoV-2_MSA_file1.fasta
+#Simulate
+#../NanoSim/src/simulator.py metagenome -gl ../config_files/metagenome_covid_human.tsv -dl ../config_files/dna_type_list.tsv -a ../config_files/abundance_covid.tsv
--- setup.sh
+++ setup.sh
@@ -17,9 +17,11 @@ downloadHumanGenome(){
 
 downloadCovid(){
 
-  #wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P .data/covid/
-  #wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P .data/covid/
-  wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P .data/covid/
+  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R1.fastq.1" -P ./data/covid/
+  wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P ./data/covid/
+  mv ./data/covid/SP-2_R1.fastq.1 ./data/covid/SP-1_R2.fastq
+  mv ./data/covid/SP-2_R2.fastq.1 ./data/covid/SP-2_R2.fastq
+  #wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P ./data/covid/
 }
 
 extract(){
