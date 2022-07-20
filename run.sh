353d322f63243b10220300e0291140b28b43ea98
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-21 00:38:42 +0200

    Path fix

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@
 
 mkdir ./"NanoSim_output"/ && cd NanoSim_output
 
+
+data_dir="$(dirname $(dirname $(realpath $0)) )/data"
+echo $data_dir
+
 #Train
-../NanoSim/src/read_analysis.py genome -i ../data/covid/SP-2_R1.fastq -rg ../data/covid/SARS-CoV-2_MSA_file1.fasta
+../NanoSim/src/read_analysis.py genome -i "$data_dir/covid/SP-2_R1.fastq" -rg "$data_dir/covid/SARS-CoV-2_MSA_file1.fasta"
 #Simulate
 #../NanoSim/src/simulator.py metagenome -gl ../config_files/metagenome_covid_human.tsv -dl ../config_files/dna_type_list.tsv -a ../config_files/abundance_covid.tsv
