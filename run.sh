fa8e0c6aa96b9b45a2562829f0c9693f68251d51
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-07-25 11:43:45 +0200

    WIP

--- run.sh
+++ run.sh
@@ -3,14 +3,11 @@
 eval "$(conda shell.bash hook)"
 conda activate nanosim
 
-mkdir ./"NanoSim_output"/
-
-
 data_dir="$(dirname $(realpath $0) )"
 echo $data_dir
 
-cd NanoSim_output
+mkdir ./"NanoSim_output"/ && cd NanoSim_output
 #Train
-../NanoSim/src/read_analysis.py genome -i "$data_dir/data/covid/SP-2_R1.fastq" -rg "$data_dir/data/covid/SARS-CoV-2_MSA_file1.fasta" && shutdown
+../NanoSim/src/read_analysis.py genome -i "$data_dir/data/covid/SP-2_R1.fastq" -rg "$data_dir/data/covid/SARS-CoV-2_MSA_file1.fasta" && poweroff
 #Simulate
 #../NanoSim/src/simulator.py metagenome -gl "$data_dir/config_files/metagenome_covid_human.tsv" -dl "$data_dir/config_files/dna_type_list.tsv" -a "$data_dir/config_files/abundance_covid.tsv"
--- setup.sh
+++ setup.sh
@@ -21,8 +21,8 @@ downloadCovid(){
   wget "https://storage.googleapis.com/nih-sequence-read-archive/sra-src/SRR12412952/SP-2_R2.fastq.1" -P ./data/covid/
   mv ./data/covid/SP-2_R1.fastq.1 ./data/covid/SP-2_R1.fastq
   mv ./data/covid/SP-2_R2.fastq.1 ./data/covid/SP-2_R2.fastq
-  #wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P ./data/covid/
-  #mv ./data/covid/5ede595708aad3013143c7f3?action=download\&direct\&version=3 ./data/covid/SARS-CoV-2_MSA_file1.fasta
+  wget "https://files.ca-1.osf.io/v1/resources/jt2n5/providers/osfstorage/5ede595708aad3013143c7f3?action=download&direct&version=3" -P ./data/covid/
+  mv ./data/covid/5ede595708aad3013143c7f3?action=download\&direct\&version=3 ./data/covid/SARS-CoV-2_MSA_file1.fasta
 }
 
 extract(){
@@ -33,16 +33,13 @@ setupNanosim(){
 
     git submodule init
     git submodule update
-    cp fixed_requirements.txt ./NanoSim/requirements.txt
-    # Solution for this ??
     eval "$(conda shell.bash hook)"
-    #conda init
     cd NanoSim && conda create --name nanosim_test python=3.7 --yes
     conda activate nanosim_test && conda install --file requirements.txt -c conda-forge -c bioconda --yes
     cd .. && conda activate base
 }
 
-#setupDirs
-#downloadHumanGenome
-#downloadCovid
+setupDirs
+downloadHumanGenome
+downloadCovid
 setupNanosim
