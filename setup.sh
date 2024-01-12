761a4fdfb546d35e3a67bf7bfebac37d1abf941f
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2024-01-12 22:48:19 +0100

    Cleanup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#./download_data.sh "$PWD/data"
-nextflow run runSimulation.nf
-#-log /home/vickylara/Documents/Covid-SpiNGS/covid-spings-pipeline/logs
-
-#nextflow run runPipeline.nf
